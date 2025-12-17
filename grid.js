26faf22c645650d2a8450472efab4ce997757fe1
nahwoah <119922465+nahwoah@users.noreply.github.com>
2025-12-17 20:03:17 +0200

    finished project

--- grid.js
+++ grid.js
@@ -1,33 +1,24 @@
-
+const body = document.querySelector("#mainBody");
 const container = document.querySelector("#container");
 const resetContainer = document.querySelector("#container1");
 const reset = document.querySelector("#reset");
 
 const resetButtonSize = 18;
-const resetButtonMargin = 5;
+const resetButtonMargin = 12;
+
+body.style.display = "flex";
+body.style.flexDirection = "column";
 
 resetContainer.style.display = "flex";
 resetContainer.style.justifyContent = "center";
 resetContainer.style.margin = 5 + "px";
-//reset.style.border = "1px solid #00ff88ff";
 container.style.display = "flex";
 container.style.justifyContent = "center";
 container.style.flexWrap = "wrap";
 
 
-
 let windowHeight = Math.floor((window.innerHeight-resetButtonSize-resetButtonMargin)/18);
 let rowWidth = Math.floor(document.documentElement.offsetWidth/18);
-
-// grid.setAttribute("style", "width:16px; height:16px;");
-//grid.style.width = 16 + "px"
-// grid.style.height = document.documentElement.offsetHeight;
-//grid.style.setProperty("height", "50px", "important");
-
-//grid.style.height = 16 + "px";
-
-//grid.style.height = "50px";
-//grid.style.border = "1px solid #00ff88ff";
 
 for (let i = 0; i < rowWidth; i++) {
     for (let j = 0; j < windowHeight; j++) {
@@ -38,68 +29,63 @@ for (let i = 0; i < rowWidth; i++) {
         grid.style.width = 16 + "px";
         grid.style.height = 16 + "px";
         grid.style.margin = 0 + "px";
-        grid.style.border = "1px solid #00ff88ff";
+        //grid.style.border = "1px solid #00ff88ff";
+        grid.style.border = "1px solid #777777ff";
+        
+        let rgbDecrement = 25.5;
+        let rgbValue = 255;
 
-        grid.addEventListener("mouseover", (event) => {grid.style.backgroundColor = "red"
-            setTimeout(() => {
-                event.target.style.backgroundColor = "";
-                }, 2000);
+        grid.addEventListener("mouseover", (event) => {
+            rgbValue = rgbValue - rgbDecrement;
+            rgbRandRed = Math.floor(Math.random() * (rgbValue));
+            rgbRandGreen = Math.floor(Math.random() * (rgbValue));
+            rgbRandBlue = Math.floor(Math.random() * (rgbValue));
+
+            grid.style.backgroundColor = `rgb(${rgbRandRed}, ${rgbRandGreen}, ${rgbRandBlue})`;
         })
     }
 }
 
-console.log(container.children.length)
-
 reset.addEventListener("click", () => {
-    let element = document.getElementById("#gridItem");
-    let numberOfChildren = element.getElementById("#gridItem").length
+    let numberOfChildren = document.querySelectorAll("#gridItem").length
     for (let i=0; i<(numberOfChildren); i++){
         const grid = document.querySelector("#gridItem");
         container.removeChild(grid);
     }
-    // let cells = prompt("Enter number of cells");
-    // for (let c = 0; c < cells; c++){
-    //     const grid = document.createElement("div");
-    //     grid.id = "gridItem";
-    //     container.appendChild(grid);
+    let cells = prompt("Enter number of cells");
+    if ((cells * 18) < window.innerHeight){
+        body.style.alignItems = "center";
+        container.style.width = (cells*18) + "px";
+        container.style.height = (cells*18) + "px";
+    } else if(cells > 150){
+        window.alert("More than 150 cells would crash the page! Please try again.");
+        return;
+    }else{
+        container.style.width = "";
+        container.style.height = "";
+    }
+    
+    for (let c = 0; c < (cells*cells); c++){
+        const grid = document.createElement("div");
+        grid.id = "gridItem";
+        container.appendChild(grid);
+        
+        grid.style.width = 16 + "px";
+        grid.style.height = 16 + "px";
+        grid.style.margin = 0 + "px";
+        //grid.style.border = "1px solid #00ff88ff";
+        grid.style.border = "1px solid #777777ff";
 
-    //     grid.style.width = 16 + "px";
-    //     grid.style.height = 16 + "px";
-    //     grid.style.margin = 0 + "px";
-    //     //grid.setAttribute("style", "width:16px; height:16px;");
-    //     grid.style.border = "1px solid #00ff88ff";
+        let rgbDecrement = 25.5;
+        let rgbValue = 255;
 
-    //     grid.addEventListener("mouseover", (event) => {grid.style.backgroundColor = "red"
-    //         setTimeout(() => {
-    //             event.target.style.backgroundColor = "";
-    //             }, 2000);
-    //     })
-    // }
+        grid.addEventListener("mouseover", (event) => {
+            rgbValue = rgbValue - rgbDecrement;
+            rgbRandRed = Math.floor(Math.random() * (rgbValue));
+            rgbRandGreen = Math.floor(Math.random() * (rgbValue));
+            rgbRandBlue = Math.floor(Math.random() * (rgbValue));
+
+            grid.style.backgroundColor = `rgb(${rgbRandRed}, ${rgbRandGreen}, ${rgbRandBlue})`;
+        })
+    }
 })
-
-
-
-
-
-// function createGrid(size) {
-//     let ratioW = Math.floor((window.innerWidth || document.documentElement.offsetWidth) / size)
-//     let ratioH = Math.floor((window.innerHeight || document.documentElement.offsetHeight) / size);
-
-//     let parent = document.createElement('div');
-//     parent.className = 'grid';
-//     parent.style.width = (ratioW * size) + 'px';
-//     parent.style.height = (ratioH * size) + 'px';
-
-//     for (let i = 0; i < ratioH; i++) {
-//         for (let p = 0; p < ratioW; p++) {
-//             let cell = document.createElement('div');
-//             cell.style.height = (size - 1) + 'px';
-//             cell.style.width = (size - 1) + 'px';
-//             parent.appendChild(cell);
-//         }
-//     }
-
-//     document.body.appendChild(parent);
-// }
-
-// createGrid(16);
